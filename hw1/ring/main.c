5fe119a7a80ff6c9ce8b2f1436621a978332c0af
acipol2 <acipol2@login-1.extreme.acer.uic.edu>
2017-09-29 01:20:49 -0500

    First commit hw1

---
+++ hw1/ring/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+#define P 16 
+#define N 32
+
+
+int main(int argc, char** argv) {
+  
+  MPI_Comm ring_comm;
+  int dim[] = {4,4};
+  int period[] = {1,1};
+  int reorder = 1;
+  int cord[2];
+  int world_size;
+  int rank;
+  int data[N];
+  int partial_data[N/P];
+  int i;
+  int result;
+  int partial_sum;
+
+  MPI_Init(&argc,&argv);
+  
+  MPI_Cart_create(MPI_COMM_WORLD,2,dim,period,reorder,&ring_comm);
+  
+  MPI_Comm_size(ring_comm, &world_size);
+
+  MPI_Comm_rank(ring_comm, &rank);
+  MPI_Cart_coords(ring_comm,rank,2,cord);
+
+  if ( cord[0] == 0 && cord[1] == 0 ) {
+    srand(NULL);
+    for ( i = 0; i < N; i++ ) {
+      //data[i] = rand() % 20;
+      data[i] = i;
+    }
+  }
+
+  MPI_Scatter(data,N/P,MPI_INT,partial_data,N/P,MPI_INT,0,ring_comm);
+
+  printf("rank: %d, cord: [%d][%d], %d, %d\n", rank, cord[0], cord[1], partial_data[0], partial_data[1]);
+
+  partial_sum = partial_data[0] + partial_data[1];
+  MPI_Reduce(&partial_sum,&result,1,MPI_INT,MPI_SUM,0,ring_comm);
+
+  if ( rank == 0 ) 
+    printf("Sum: %d\n", result);
+
+  MPI_Finalize();
+
+  return 0;
+  
+}
+
+
