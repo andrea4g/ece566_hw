e58ffaa6be0a75d206e22040aa10660ac8b89427
acipol2 <acipol2@login-1.extreme.acer.uic.edu>
2017-10-05 00:34:52 -0500

    almost done also ring send rcv

--- hw1/ring/main_ring.c
+++ hw1/ring/main_ring.c
@@ -6,21 +6,20 @@
 int main(int argc, char** argv) {
 
   MPI_Comm ring_comm;
-  int mailbox;
+  unsigned int mailbox,reminder;
   int period = 1;
   int reorder = 1;
-  int my_rank, my_cord;
+  int my_rank, my_coord;
   int k,n;
-  int* data;
-  int* partial_data;
-  int card_partial_data;
+  unsigned int* data;
+  unsigned int* partial_data;
+  unsigned int card_partial_data;
   int i;
   int root_cord,root_rank;
-  int result;
-  int partial_sum = 0;
+  long unsigned int partial_sum = 0;
   double initial_time, final_time;
-  int dest_cord,dest_rank,error_value,source_cord;
-  int temp;
+  int dst_coord,src_coord;
+  int src_rank,dst_rank;
   MPI_Status status;
 
 
@@ -32,38 +31,45 @@ int main(int argc, char** argv) {
 
   MPI_Comm_size(MPI_COMM_WORLD, &k);
 
+  card_partial_data = n/k;
+  reminder = n % k;
+
   MPI_Cart_create(MPI_COMM_WORLD, 1, &k, &period, reorder, &ring_comm);
   
-  MPI_Comm_rank(ring_comm, &rank);
-  MPI_Cart_coords(ring_comm, rank, 1, &my_cord);
- 
-  number_of_steps = 0;
-  while ( (1 << number_of_steps) < k ) {
-    number_of_steps++;
-  }  
+  MPI_Comm_rank(ring_comm, &my_rank);
+  MPI_Cart_coords(ring_comm, my_rank, 1, &my_coord);
   
-  data = (int *)malloc(n*sizeof(int));
-  if ( my_cord == 0 ) {
-    for ( i = 0; i < n; i++ )
-      data[i] = i;
-  }
-
 
   if ( my_coord == 0 ) {
-    MPI_Cart_shift(ring_comm, 0, 1, &my_rank, &dest_rank);
+    data = (unsigned int *)malloc(n*sizeof(int));
+    for ( i = 0; i < n; i++ )
+      data[i] = i;
+  } else {
+    partial_data = (unsigned int *) malloc ( (k-my_coord)*card_partial_data*sizeof(int) );
+  }
+
+  MPI_Cart_shift(ring_comm, 0, +1, &my_rank, &dst_rank);
+  MPI_Cart_shift(ring_comm, 0, -1, &my_rank, &src_rank);
+
+  if ( my_coord == 0 ) {
+    dst_coord = 1;
+    MPI_Cart_rank(ring_comm, &dst_coord, &dst_rank);
     MPI_Send(&data[card_partial_data + reminder], 
-        (k-1)*card_partial_data, MPI_INT, dest_rank, 0, ring_comm);
+        (k-1)*card_partial_data, MPI_INT, dst_rank, 0, ring_comm);
     partial_sum = 0;
     for ( i = 0 ; i < card_partial_data + reminder; i++ ) {
       partial_sum += data[i];
     }
   } else {
-    MPI_Cart_shift(ring_comm, 0, -1, &my_rank, &src_rank);
+    src_coord = my_coord - 1;
+    MPI_Cart_rank(ring_comm, &src_coord, &src_rank);
     MPI_Recv(partial_data, 
         (k-my_coord)*card_partial_data, MPI_INT, src_rank, 0, ring_comm, &status);
-    if ( my_coord < k ) {
+    if ( my_coord < (k-1) ) {  
+      dst_coord = (my_coord + 1) % k;
+      MPI_Cart_rank(ring_comm, &dst_coord, &dst_rank);
       MPI_Send(&partial_data[card_partial_data], 
-          (k - my_cord - 1)*card_partial_data, MPI_INT, dest_rank, 0, ring_comm);
+          (k - my_coord - 1)*card_partial_data, MPI_INT, dst_rank, 0, ring_comm);
     }
     partial_sum = 0;
     for ( i = 0 ; i < card_partial_data; i++ ) {
@@ -71,35 +77,36 @@ int main(int argc, char** argv) {
     }
   }
 
-  MPI_Cart_rank(ring_comm,&root_cord,&root_rank);
-
-  if ( my_cord == N/2 ) {
-    MPI_Cart_shift(ring_comm, 0, -1, &my_rank, &dest_rank);
-    MPI_Send(&partial_sum, 1, MPI_INT, dest_rank, 0, ring_comm);
-  } else if ( my_cord == N/2 + 1) {
-    MPI_Cart_shift(ring_comm, 0, +1, &my_rank, &dest_rank);
-    MPI_Send(&partial_sum, 1, MPI_INT, dest_rank, 0, ring_comm);
-  } else if ( my_cord == 0 ) {
-    MPI_Cart_shift(ring_comm, 0, -1, &my_rank, &src_rank);
+  if ( my_coord == k/2  ) {
+    dst_coord = my_coord - 1;
+    MPI_Cart_rank(ring_comm, &dst_coord, &dst_rank);
+    MPI_Send(&partial_sum, 1, MPI_INT, dst_rank, 0, ring_comm);
+  } else if ( my_coord == k/2 + 1 ) {
+    dst_coord = my_coord + 1;
+    MPI_Cart_rank(ring_comm, &dst_coord, &dst_rank);
+    MPI_Send(&partial_sum, 1, MPI_INT, dst_rank, 0, ring_comm);
+  } else if ( my_coord == 0 ) {
+    src_coord = my_coord + 1;
+    MPI_Cart_rank(ring_comm, &src_coord, &src_rank);
     MPI_Recv(&mailbox, 1, MPI_INT, src_rank, 0, ring_comm, &status);
     partial_sum += mailbox;
-    MPI_Cart_shift(ring_comm, 0, +1, &my_rank, &src_rank);
+    src_coord = (my_coord - 1) % k;
+    MPI_Cart_rank(ring_comm, &src_coord, &src_rank);
     MPI_Recv(&mailbox, 1, MPI_INT, src_rank, 0, ring_comm, &status);
     partial_sum += mailbox;
   } else {
-      MPI_Cart_shift(ring_comm, 0,
-        cord < N/2 ? +1 : -1, &my_rank, &src_rank);
-      MPI_Cart_shift(ring_comm, 0,
-        cord < N/2 ? -1 : +1, &my_rank, &dst_rank);
-      MPI_Recv(&mailbox, 1, MPI_INT, src_rank, 0, ring_comm, &status);
-      partial_sum += mailbox;
-      MPI_Send(&mailbox, 1, MPI_INT, dst_rank, 0, ring_comm, &status);
+    src_coord = my_coord < k/2 ? (my_coord + 1) % k : (my_coord - 1) % k;
+    dst_coord = my_coord < k/2 ? (my_coord - 1) % k : (my_coord + 1) % k;
+    MPI_Cart_rank(ring_comm, &src_coord, &src_rank);
+    MPI_Cart_rank(ring_comm, &dst_coord, &dst_rank);
+    MPI_Recv(&mailbox, 1, MPI_INT, src_rank, 0, ring_comm, &status);
+    partial_sum += mailbox;
+    MPI_Send(&partial_sum, 1, MPI_INT, dst_rank, 0, ring_comm);
   }
 
-  if ( my_cord == 0 ) {
-    printf("%d\n", partial_sum);
+ if ( my_coord == 0 ) {
+    printf("%lu\n", partial_sum);
   }
-
 
   MPI_Finalize();
 
