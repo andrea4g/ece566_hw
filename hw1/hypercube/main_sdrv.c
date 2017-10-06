2721a45be1baf9e806cd40f40f6ce28fc9e5983b
cancro7 <antonio.cipolletta7@gmail.com>
2017-10-06 15:52:51 -0500

    added iterations to compute time main_sdrv hc

--- hw1/hypercube/main_sdrv.c
+++ hw1/hypercube/main_sdrv.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <mpi.h>
 
+#define DEBUG 1
+#define N_ITERATIONS 1000
+
+
 int main(int argc, char** argv) {
 
   // variable declaration
@@ -19,13 +23,15 @@ int main(int argc, char** argv) {
   int world_size;
   int actual_rank;
   int save;
+  int iteration;
   unsigned int card_partial_data;
   unsigned int d;
   unsigned int bit;
   unsigned int n;
   unsigned int temp;
   char flag;
-  double initial_time, final_time;
+  double time_vector[N_ITERATIONS],deviation;
+  double average_time, final_time, initial_time;
   // pointer declaration
   int* cord;
   int* c;
@@ -103,91 +109,111 @@ int main(int argc, char** argv) {
     my_cord += save;
   }
 
-  // get the rank of each processor in the topology
+  // get the rank of the root in the topology
   MPI_Cart_rank(hc_comm, root_cord, &root_rank);
   // 1-to-all personalized from source to all the processors
   // implemented using send and receive
-  mask = world_size - 1;
-  index = n / 2;
-  for (i = d - 1; i >= 0; i--) {
-    mask = mask ^ (1 << i);
-    if ((my_cord & mask) == 0) {
-      if ((my_cord & (1 << i)) == 0) {
-        virtual_dest_cord = my_cord ^ (1 << i);
-        temp1 = virtual_dest_cord;
-        for (k = 0; k < d; k++) {
-          bit = virtual_dest_cord & 0x01;
-          c[k] = bit;
-          virtual_dest_cord = virtual_dest_cord >> 1;
+  
+  for ( iteration = 0; iteration < N_ITERATIONS; iteration++ ) { 
+    
+    initial_time = MPI_Wtime();
+    mask = world_size - 1;
+    index = n / 2;
+    for (i = d - 1; i >= 0; i--) {
+      mask = mask ^ (1 << i);
+      if ((my_cord & mask) == 0) {
+        if ((my_cord & (1 << i)) == 0) {
+          virtual_dest_cord = my_cord ^ (1 << i);
+          temp1 = virtual_dest_cord;
+          for (k = 0; k < d; k++) {
+            bit = virtual_dest_cord & 0x01;
+            c[k] = bit;
+            virtual_dest_cord = virtual_dest_cord >> 1;
+          }
+          MPI_Cart_rank(hc_comm, c, &actual_rank);
+          MPI_Send(&data[index], index, MPI_INT, actual_rank, 0 , hc_comm);
+          }
+        else {
+          virtual_source_cord = my_cord ^ (1 << i);
+          temp1 = virtual_source_cord;
+          for (k = 0; k < d; k++) {
+            bit = virtual_source_cord  & 0x01;
+            c[k] = bit;
+            virtual_source_cord = virtual_source_cord >> 1;
+          }
+          MPI_Cart_rank(hc_comm, c, &actual_rank);
+          MPI_Recv(data, index, MPI_INT, actual_rank, 0, hc_comm, MPI_STATUS_IGNORE);
         }
-        MPI_Cart_rank(hc_comm, c, &actual_rank);
-        MPI_Send(&data[index], index, MPI_INT, actual_rank, 0 , hc_comm);
-        }
-      else {
-        virtual_source_cord = my_cord ^ (1 << i);
-        temp1 = virtual_source_cord;
-        for (k = 0; k < d; k++) {
-          bit = virtual_source_cord  & 0x01;
-          c[k] = bit;
-          virtual_source_cord = virtual_source_cord >> 1;
-        }
-        MPI_Cart_rank(hc_comm, c, &actual_rank);
-        MPI_Recv(data, index, MPI_INT, actual_rank, 0, hc_comm, MPI_STATUS_IGNORE);
       }
+      index = index >> 1;
     }
-    index = index >> 1;
-  }
 
-  // save the final value of the scatter inside partial_data
-  for (i = 0; i < card_partial_data; i++) {
-    partial_data[i] = data[i];
-  }
-  // initialize the partial sum of each processor
-  partial_sum = 0;
-  // compute the sum of all the values received with the scatter
-  for (i = 0 ; i < card_partial_data; i++)
-    partial_sum += partial_data[i];
-
-  // apply reduce operation (MPI_SUM) on the root processor
-  // implemented using send and receive
-  mask = 0;
-  for (i = 0; i < d; i++) {
-    if ((my_cord & mask) == 0) {
-      if ((my_cord & (1 << i)) != 0) {
-        msg_dest = my_cord ^ (1 << i);
-        for (k = 0; k < d; k++) {
-          bit =  msg_dest & 0x01;
-          c[k] = bit;
-          msg_dest = msg_dest >> 1;
-        }
-        MPI_Cart_rank(hc_comm, c, &actual_rank);
-        MPI_Send(&partial_sum, 1, MPI_INT, actual_rank, 0, hc_comm);
-      }
-      else {
-        msg_source = my_cord ^ (1 << i);
-        for (k = 0; k < d; k++) {
-          bit =  msg_source & 0x01;
-          c[k] = bit;
-          msg_source = msg_source >> 1;
-        }
-        MPI_Cart_rank(hc_comm, c, &actual_rank);
-        MPI_Recv(&acc, 1, MPI_INT, actual_rank, 0, hc_comm, MPI_STATUS_IGNORE);
-        partial_sum += acc;
-      }
+    // save the final value of the scatter inside partial_data
+    for (i = 0; i < card_partial_data; i++) {
+      partial_data[i] = data[i];
     }
-    mask = mask ^ (1 << i);
-  }
-  // save the final result of the reduction inside result
-  result = partial_sum;
+    // initialize the partial sum of each processor
+    partial_sum = 0;
+    // compute the sum of all the values received with the scatter
+    for (i = 0 ; i < card_partial_data; i++)
+      partial_sum += partial_data[i];
 
-  // if it is the root processor print the sum
-  if (flag){
-    printf("Sum: %d\n", result);
+    // apply reduce operation (MPI_SUM) on the root processor
+    // implemented using send and receive
+    mask = 0;
+    for (i = 0; i < d; i++) {
+      if ((my_cord & mask) == 0) {
+        if ((my_cord & (1 << i)) != 0) {
+          msg_dest = my_cord ^ (1 << i);
+          for (k = 0; k < d; k++) {
+            bit =  msg_dest & 0x01;
+            c[k] = bit;
+            msg_dest = msg_dest >> 1;
+          }
+          MPI_Cart_rank(hc_comm, c, &actual_rank);
+          MPI_Send(&partial_sum, 1, MPI_INT, actual_rank, 0, hc_comm);
+        }
+        else {
+          msg_source = my_cord ^ (1 << i);
+          for (k = 0; k < d; k++) {
+            bit =  msg_source & 0x01;
+            c[k] = bit;
+            msg_source = msg_source >> 1;
+          }
+          MPI_Cart_rank(hc_comm, c, &actual_rank);
+          MPI_Recv(&acc, 1, MPI_INT, actual_rank, 0, hc_comm, MPI_STATUS_IGNORE);
+          partial_sum += acc;
+        }
+      }
+      mask = mask ^ (1 << i);
+    }
+    // save the final result of the reduction inside result
+    result = partial_sum;
+
+    final_time = MPI_Wtime();
+    // if it is the root processor print the sum
+#if DEBUG
+    if (flag){
+      printf("Sum: %d\n", result);
+#endif
+    }
+    // save final time of the task
+    final_time = MPI_Wtime();
+    time_vector[iteration] = final_time - initial_time;
+    average_time += time_vector[iteration];
+    // compute and print the rank of the processor and the time it took to complete the task 
+  } 
+  average_time = average_time/N_ITERATIONS;
+  
+  if ( flag ) {
+    deviation = 0;
+    for ( i = 0; i < N_ITERATIONS; i++ ) {
+      deviation += (time_vector[i] - average_time)*(time_vector[i] - average_time);
+    }
+    // compute and print the rank of the processor and the time it took to complete the task
+    printf("Av_time: %f ,dev: %f\n", average_time, deviation);
   }
-  // save final time of the task
-  final_time = MPI_Wtime();
-  // compute and print the rank of the processor and the time it took to complete the task
-  printf("Proc: %d, time: %f\n", rank, final_time-initial_time);
+
   // free the dynamic memory allocated
   free(cord);
   free(c);
