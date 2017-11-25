10f6f161530f19e23f6554d91a666ee39e9d7ed8
andrea4g <andrea.gualco@gmail.com>
2017-11-25 12:02:12 -0600

    hw4 first commit

---
+++ hw3/DNS/output/generator_output.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm *.err
+
+filename="$(ls *.out)";
+
+cat /dev/null > "out.txt"
+for file in $filename; do
+  echo -n "$file, " >> "out.txt";
+  cat $file | head -1 >> "out.txt";
+done
